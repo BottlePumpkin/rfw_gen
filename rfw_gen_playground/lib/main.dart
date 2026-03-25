a158cd5087534c1cda8c7812f6d5f34c3cbb4c4a
BottlePumpkin <p4569zz@gmail.com>
2026-03-25 20:09:39 +0900

    chore(playground): create web docs project scaffold

---
+++ rfw_gen_playground/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'rfw_gen Docs',
+    home: Scaffold(body: Center(child: Text('rfw_gen_playground'))),
+  ));
+}
